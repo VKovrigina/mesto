f6c1b8cf3ade7709206623a5a24d4fae5616fcc7
Varvara Kovrigina <Onlimusik@yandex.ru>
2020-05-23 13:24:56 +0300

    feat: popup place

--- script/script.js
+++ script/script.js
@@ -2,13 +2,18 @@ const buttonEdit = document.querySelector('.profile__edit-button');
 const buttonClose = document.querySelector('.popup__close-button');
 const formElement = document.querySelector('.popup__form');
 const popup = document.querySelector('.popup');
+//модификатор для попапа с картачками
+const popupPlace = document.querySelector('.popup_place');
+const buttonAdd = document.querySelector('.profile__add-button');
+//модификатор для книпки закрыть
+const buttonClosePlace = document.querySelector('.popup__close-button_place');
 //поля формы
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_job');
 // Выберите элементы, куда должны быть вставлены значения полей
 let profileNameInput = document.querySelector('.profile__name');
 let profileJobInput = document.querySelector('.profile__job');
-//сонтэйнер для карточек
+//контэйнер для карточек
 const cardContainer = document.querySelector('.cards');
 const initialCards = [
   {
@@ -37,6 +42,11 @@ const initialCards = [
   }
 ];
 
+//функция закрытия-открытия попапа для добавления карточек
+function popupOpenClosePlace() {
+  popupPlace.classList.toggle('popup_open-place');
+}
+//функция добавления карточек
 function addCard(imgValue, titleValue) {
   //выбрали шаблон
   const cardTemplate = document.querySelector('#card-template').content;
@@ -75,3 +85,5 @@ function formSubmitHandler (evt) {
 buttonEdit.addEventListener('click', popupOpenClose);
 formElement.addEventListener('submit', formSubmitHandler);
 buttonClose.addEventListener('click', popupOpenClose);
+buttonAdd.addEventListener('click', popupOpenClosePlace);
+buttonClosePlace.addEventListener('click', popupOpenClosePlace);
