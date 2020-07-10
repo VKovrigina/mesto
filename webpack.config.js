a7a91908a9334a4660c5f2bf33d151c76ef3c138
Varvara Kovrigina <Onlimusik@yandex.ru>
2020-07-10 21:14:00 +0300

    feature:  add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path'); // подключаем path к конфигу вебпак
 
 module.exports = {
-    entry: { main: './src/pages/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    }
-}
+  entry: { main: './src/pages/index.js' },
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      }
+      ]
+  }
+};
