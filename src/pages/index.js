e84d58b4132aecf857ce505902eae807da25923d
Varvara Kovrigina <Onlimusik@yandex.ru>
2020-07-22 19:18:01 +0300

    fix: add button Avatar

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,10 @@ import {
   popupPhotoTitleSelector,
   popupProfileAvatarSelector,
   avatarSelector,
+  avatarButtonSelector,
   profileNameSelector,
-  profileAboutSelector} from '../utils/constants.js';
+  profileAboutSelector,
+  apiOptions} from '../utils/constants.js';
 
 import { FormValidator } from '../components/FormValidator.js';
 import Section from '../components/Section.js';
@@ -44,7 +46,7 @@ const popupProfileAvatar = document.querySelector(popupProfileAvatarSelector);
 /** кнопки */
 const buttonEdit = document.querySelector(buttonEditSelector);
 const buttonAdd = document.querySelector(buttonAddSelector);
-const avatarImg = document.querySelector(avatarSelector);
+const avatarButton = document.querySelector(avatarButtonSelector);
 /** поля форм */
 const nameInput = formProfileElement.querySelector(nameInputSelector);
 const jobInput = formProfileElement.querySelector(jobInputSelector);
@@ -53,13 +55,7 @@ const popupPhotoImg = document.querySelector(popupPhotoImgSelector);
 const popupPhotoTitle = document.querySelector(popupPhotoTitleSelector);
 
 /** API */
-const api = new Api({
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-13',
-  headers: {
-    authorization: '64f8b885-2658-44d6-bc45-dfe390bdb5b7',
-    'Content-Type': 'application/json'
-  }
-});
+const api = new Api(apiOptions);
 
 
 const userInfo = new UserInfo(profileNameSelector, profileAboutSelector, avatarSelector);
@@ -214,7 +210,7 @@ buttonEdit.addEventListener('click', () => {
   addActualMeaningProfileForm();
   popupProfile.open();
 });
-avatarImg.addEventListener('click', () => {
+avatarButton.addEventListener('click', () => {
   popupAvatar.open();
 });
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -20,6 +20,7 @@ export const cardContainer = '.cards';
 export const popupPhotoImgSelector = '.popup__img';
 export const popupPhotoTitleSelector = '.popup__photo-title';
 export const avatarSelector = '.profile__avatar';
+export const avatarButtonSelector = '.profile__avatar-button';
 export const profileNameSelector = '.profile__name';
 export const profileAboutSelector = '.profile__job';
 
@@ -28,4 +29,12 @@ export const formValidationOptions = {
   submitButtonSelector: '.popup__form-button',
   inactiveButtonClass: 'popup__form-button_inactive',
   inputErrorClass: 'popup__input_type_error'
+};
+
+export const apiOptions = {
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-13',
+  headers: {
+    authorization: '64f8b885-2658-44d6-bc45-dfe390bdb5b7',
+    'Content-Type': 'application/json'
+  }
 };
